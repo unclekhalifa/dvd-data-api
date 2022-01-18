d1ad665493b354e8f390880bd56ef1e196059a16
khalifa <ibrahim.idris.developer@gmail.com>
2022-01-18 07:11:09 +0000

    initialise project with npm and eslint

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+app.get('*', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(process.env.PORT);
